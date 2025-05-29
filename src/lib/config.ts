95dd9fb711d7a9c6d444291f4c17e4fa05e1bcaa
luxapientia <trongtruong.pham192@gmail.com>
2025-05-29 10:12:49 +0700

    define file max size in the db config

--- src/lib/config.ts
+++ src/lib/config.ts
@@ -1,6 +1,7 @@
 export const DATABASE_CONFIG = {
   uri: process.env.MONGODB_URI,
   name: process.env.MONGODB_DB,
+  fileMaxSize: process.env.NEXT_PUBLIC_FILE_MAX_SIZE ? parseInt(process.env.NEXT_PUBLIC_FILE_MAX_SIZE) * 1024 * 1024 : 2 * 1024 * 1024,
   options: {
     maxPoolSize: 10,
     minPoolSize: 5,
