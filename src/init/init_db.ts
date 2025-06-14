dbb46272b328986e35119a98f46614676b215a36
luxapientia <trongtruong.pham192@gmail.com>
2025-06-14 17:36:23 +0700

    Add wallet initialization logic in database setup

--- src/init/init_db.ts
+++ src/init/init_db.ts
@@ -3,6 +3,7 @@ import { TeamCommisionLevelModel } from '../models/TeamCommisionLevel';
 import { TrustPlanModel } from '../models/TrustPlan';
 import { InterestMatrixModel } from '../models/InterestMatrix';
 import { InvitationSettingModel } from '../models/InvitationSetting';
+import { WalletModel } from '../models/Wallet';
 
 
 import app_config from '../../data/init/appConfig.json';
@@ -10,6 +11,8 @@ import team_commision_levels from '../../data/init/teamCommisionLevels.json';
 import trust_plan from '../../data/init/trustPlan.json';
 import interest_matrix from '../../data/init/interestMatrix.json';
 import invitation_setting from '../../data/init/invitationSetting.json';
+import { walletService } from '../services/Wallet';
+import { encryptPrivateKey } from '../utils/encrypt';
 
 const init_db = async () => {
     const appConfig = await AppConfigModel.findOne({});
@@ -33,6 +36,35 @@ const init_db = async () => {
     const invitationSetting = await InvitationSettingModel.findOne({});
     if (!invitationSetting) {
         await InvitationSettingModel.create(invitation_setting);
+    }
+
+    const walletAddresses = await WalletModel.find({});
+    const ethereumWallet = walletAddresses.find(wallet => wallet.chain === 'Ethereum');
+    if (!ethereumWallet) {
+        const newEthereumWallet = await walletService.generateWalletCredentials('Ethereum');
+        await WalletModel.create({
+            address: newEthereumWallet.address,
+            privateKeyEncrypted: encryptPrivateKey(newEthereumWallet.privateKey),
+            chain: 'Ethereum'
+        });
+    }
+    const tronWallet = walletAddresses.find(wallet => wallet.chain === 'Tron');
+    if (!tronWallet) {
+        const newTronWallet = await walletService.generateWalletCredentials('Tron');
+        await WalletModel.create({
+            address: newTronWallet.address,
+            privateKeyEncrypted: encryptPrivateKey(newTronWallet.privateKey),
+            chain: 'Tron'
+        });
+    }
+    const bscWallet = walletAddresses.find(wallet => wallet.chain === 'Binance');
+    if (!bscWallet) {
+        const newBscWallet = await walletService.generateWalletCredentials('Binance');
+        await WalletModel.create({
+            address: newBscWallet.address,
+            privateKeyEncrypted: encryptPrivateKey(newBscWallet.privateKey),
+            chain: 'Binance'
+        });
     }
 }
 
