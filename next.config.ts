5d06a3c5259e394c3e096ea025c0871fce3619ad
luxapientia <trongtruong.pham192@gmail.com>
2025-06-14 22:58:03 +0700

    fix Nextconfig

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,13 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  webpack: (config, { isServer }) => {
+    if (isServer) {
+      config.externals = config.externals || [];
+      config.externals.push('ethers'); // ⬅️ Ignore this module during server compilation
+    }
+    return config;
+  },
 };
 
 export default nextConfig;
