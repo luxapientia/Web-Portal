1c2de8cd053477c3b9ba2b7d6148e6e7511fba14
luxapientia <trongtruong.pham192@gmail.com>
2025-06-23 04:30:15 +0700

    Refactor deposit route and page: Remove unused imports and streamline the deposit page component for improved performance and clarity.

--- src/app/api/wallet/deposit/route.ts
+++ src/app/api/wallet/deposit/route.ts
@@ -1,13 +1,11 @@
 import { NextRequest, NextResponse } from 'next/server';
-import { CentralWalletModel } from '@/models/CentralWallet';
 import { Transaction, TransactionModel } from '@/models/Transaction';
 import { authOptions, config } from '@/config';
 import { getServerSession } from 'next-auth';
 import { walletService } from '@/services/Wallet';
 import { UserModel } from '@/models/User';
 import { User } from '@/models/User';
-import { CryptoPriceModel } from '@/models/CryptoPrice';
-import { DepositWallet, DepositWalletModel, DepositWalletWithoutKeys } from '@/models/DepositWallet';
+import { DepositWallet, DepositWalletModel } from '@/models/DepositWallet';
 import { encryptPrivateKey } from '@/utils/encrypt';
 
 export async function GET() {
@@ -16,13 +14,6 @@ export async function GET() {
         if (!session?.user?.email) {
             return NextResponse.json({ error: 'Unauthorized' }, { status: 401 });
         }
-
-        const wallets = await DepositWalletModel.find({userId: session.user.id, available: true}) as DepositWallet[];
-
-        const walletWithoutKeys = wallets.map(w => ({
-            address: w.address,
-            chain: w.chain,
-        })) as DepositWalletWithoutKeys[];
 
         const supportedChains = Object.keys(config.wallet.supportedChains).map(chain => {
             const tokens = config.wallet.supportedChains[chain as keyof typeof config.wallet.supportedChains].supportedTokens.map(val => val.token);
