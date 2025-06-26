b078937b20310ed187745901b70d3fe82c3fcde2
luxapientia <trongtruong.pham192@gmail.com>
2025-06-27 03:09:42 +0700

    Refactor transaction status checking in deposit cron job: Integrate CentralWallet model for wallet validation and streamline success/failure handling based on token balance and transaction timing.

--- src/cron-job/deposit.ts
+++ src/cron-job/deposit.ts
@@ -1,8 +1,7 @@
 import cron from 'node-cron';
 import { Transaction, TransactionModel } from '../models/Transaction';
 import { walletService } from '../services/Wallet';
-import { CryptoPriceModel } from '../models/CryptoPrice';
-import { deposit } from '../controllers';
+import { CentralWallet, CentralWalletModel } from '@/models/CentralWallet';
 
 // Function to check transaction status
 async function checkPendingTransactions() {
@@ -17,58 +16,82 @@ async function checkPendingTransactions() {
 
         for (const transaction of pendingTransactions) {
             try {
-                // Check transaction status on blockchain
-                const txDetails = await walletService.getTxDetails(
-                    transaction.transactionId,
-                    transaction.chain as 'Binance' | 'Ethereum' | 'Tron'
-                );
-
-                switch (txDetails.status) {
-                    case 'success':
-                        // Update transaction status to in_review for admin approval
-                        transaction.status = 'success';
-                        if (txDetails.amount) {
-                            transaction.amount = txDetails.amount;
-                        }
-
-                        if (txDetails.toAddress !== transaction.toAddress && txDetails.token !== transaction.token) {
-                            transaction.status = 'failed';
-                            transaction.rejectReason = `The token ${txDetails.token} you provided is not the same as the token ${transaction.token} we provided and the wallet address ${transaction.toAddress} you sent the funds to is not the same as the wallet address we provided`;
-                        } else if (txDetails.token !== transaction.token) {
-                            transaction.status = 'failed';
-                            transaction.rejectReason = `The token ${txDetails.token} you provided is not the same as the token ${transaction.token} we provided`;
-                        } else if (txDetails.toAddress !== transaction.toAddress) {
-                            transaction.status = 'failed';
-                            transaction.rejectReason = `The wallet address ${transaction.toAddress} you sent the funds to is not the same as the wallet address we provided`;
-                        }
-
-
-                        if (transaction.status === 'success') {
-                            const cryptoPrice = await CryptoPriceModel.find({ symbol: txDetails.token || 'USDT' }).sort({ timestamp: -1 }).limit(1);
-                            transaction.fromAddress = txDetails.fromAddress;
-                            transaction.token = txDetails.token || 'USDT';
-                            transaction.amount = txDetails.amount || 0;
-                            transaction.amountInUSD = transaction.amount * (cryptoPrice[0]?.price || 1);
-                            await deposit(transaction.fromUserId as string, transaction.amountInUSD);
-                            transaction.releaseDate = new Date();
-                        }
-
-                        await transaction.save();
-                        break;
-
-                    case 'failed':
-                        // Mark transaction as failed
-                        transaction.status = 'failed';
-                        transaction.remarks = 'Transaction failed on blockchain';
-                        await transaction.save();
-                        console.log(`[${new Date().toISOString()}] Transaction ${transaction.transactionId} failed on blockchain.`);
-                        break;
-
-                    case 'pending':
-                        // Still pending, just log
-                        console.log(`[${new Date().toISOString()}] Transaction ${transaction.transactionId} still pending.`);
-                        break;
+                const wallet = await CentralWalletModel.findOne({ address: transaction.toAddress, chain: transaction.chain }) as CentralWallet;
+                if (!wallet) {
+                    console.log(`[${new Date().toISOString()}] Wallet not found for transaction ${transaction.transactionId}`);
+                    continue;
                 }
+
+                const tokenAmount = await walletService.getBalance(wallet.address, wallet.chain as 'Binance' | 'Ethereum' | 'Tron', transaction.token);
+
+                if (tokenAmount > wallet.startAmount) {
+                    transaction.status = 'success';
+                    transaction.releaseDate = new Date();
+                    await transaction.save();
+                    wallet.isInUse = false;
+                    wallet.startAmount = tokenAmount;
+                    await wallet.save();
+                }
+
+                if (transaction.startDate && transaction.startDate.getTime() + 1000 * 60 * 30 < new Date().getTime()) {
+                    transaction.status = 'failed';
+                    transaction.rejectReason = 'The transaction has not been completed within 30 minutes';
+                    await transaction.save();
+                    wallet.isInUse = false;
+                    await wallet.save();
+                }
+                // // Check transaction status on blockchain
+                // const txDetails = await walletService.getTxDetails(
+                //     transaction.transactionId,
+                //     transaction.chain as 'Binance' | 'Ethereum' | 'Tron'
+                // );
+
+                // switch (txDetails.status) {
+                //     case 'success':
+                //         // Update transaction status to in_review for admin approval
+                //         transaction.status = 'success';
+                //         if (txDetails.amount) {
+                //             transaction.amount = txDetails.amount;
+                //         }
+
+                //         if (txDetails.toAddress !== transaction.toAddress && txDetails.token !== transaction.token) {
+                //             transaction.status = 'failed';
+                //             transaction.rejectReason = `The token ${txDetails.token} you provided is not the same as the token ${transaction.token} we provided and the wallet address ${transaction.toAddress} you sent the funds to is not the same as the wallet address we provided`;
+                //         } else if (txDetails.token !== transaction.token) {
+                //             transaction.status = 'failed';
+                //             transaction.rejectReason = `The token ${txDetails.token} you provided is not the same as the token ${transaction.token} we provided`;
+                //         } else if (txDetails.toAddress !== transaction.toAddress) {
+                //             transaction.status = 'failed';
+                //             transaction.rejectReason = `The wallet address ${transaction.toAddress} you sent the funds to is not the same as the wallet address we provided`;
+                //         }
+
+
+                //         if (transaction.status === 'success') {
+                //             const cryptoPrice = await CryptoPriceModel.find({ symbol: txDetails.token || 'USDT' }).sort({ timestamp: -1 }).limit(1);
+                //             transaction.fromAddress = txDetails.fromAddress;
+                //             transaction.token = txDetails.token || 'USDT';
+                //             transaction.amount = txDetails.amount || 0;
+                //             transaction.amountInUSD = transaction.amount * (cryptoPrice[0]?.price || 1);
+                //             await deposit(transaction.fromUserId as string, transaction.amountInUSD);
+                //             transaction.releaseDate = new Date();
+                //         }
+
+                //         await transaction.save();
+                //         break;
+
+                //     case 'failed':
+                //         // Mark transaction as failed
+                //         transaction.status = 'failed';
+                //         transaction.remarks = 'Transaction failed on blockchain';
+                //         await transaction.save();
+                //         console.log(`[${new Date().toISOString()}] Transaction ${transaction.transactionId} failed on blockchain.`);
+                //         break;
+
+                //     case 'pending':
+                //         // Still pending, just log
+                //         console.log(`[${new Date().toISOString()}] Transaction ${transaction.transactionId} still pending.`);
+                //         break;
+                // }
             } catch (error) {
                 console.error(`[${new Date().toISOString()}] Error checking transaction ${transaction.transactionId}:`, error);
                 // Continue with next transaction even if one fails
